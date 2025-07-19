84549c305d7377ac44ff817f920ead738ad2aa54
Anthony Marquez <armarquez@gmail.com>
2025-07-19 08:35:29 -0700

    Adds tailscale-routes-fix as a submodule

--- git-init.sh
+++ git-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sets up a pre-commit hook to ensure that vault.yaml is encrypted
+# sets up a pre-commit hook to ensure that secrets.yaml is encrypted
 #
 # credit goes to nick busey from homelabos for this neat little trick
 # https://gitlab.com/NickBusey/HomelabOS/-/issues/355
@@ -10,7 +10,7 @@ if [ -d .git/ ]; then
     fi
 
     cat <<'EOT' >> .git/hooks/pre-commit
-if ( git show :ansible/vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
+if ( git show :ansible/group_vars/secrets.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
     echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
 else
     echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
